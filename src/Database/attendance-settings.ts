44b6fccdb477706a2baa4f518e6c86e7f73d3c2a
MD Rony Mia <rony.mia7800@gmail.com>
2025-07-28 16:03:59 +0600

    leave settings model added

--- src/Database/attendance-settings.ts
+++ src/Database/attendance-settings.ts
@@ -4,7 +4,7 @@ export const attendanceSettings = {
   work_availability_definition: 80,
   punch_in_out_alert: true,
   punch_in_out_interval: 1,
-  // alert_area: ['web', 'system'],
   auto_approval: false,
   is_geolocation_enabled: false,
 };
+// alert_area: ['web', 'system'],
---
+++ src/Database/leave-settings.ts
@@ -0,0 +1,7 @@
+// approval_level: 'multiple',
+// allow_bypass: 1,
+export const leaveSettings = {
+  businessId: null,
+  startMonth: 1,
+  autoApproval: false,
+};
