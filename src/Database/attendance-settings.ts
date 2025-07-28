49a460b49b31d57133e3c7609c25aa5a8ac035e6
MD Rony Mia <rony.mia7800@gmail.com>
2025-07-28 15:51:50 +0600

    added attendance settings

---
+++ src/Database/attendance-settings.ts
@@ -0,0 +1,10 @@
+export const attendanceSettings = {
+  business_id: null,
+  punch_in_time_tolerance: 15,
+  work_availability_definition: 80,
+  punch_in_out_alert: true,
+  punch_in_out_interval: 1,
+  // alert_area: ['web', 'system'],
+  auto_approval: false,
+  is_geolocation_enabled: false,
+};
